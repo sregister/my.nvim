a0e01bbf4dbfd3d9308f68999610f053452197c7
Register, Scott <scott.register@intel.com>
2024-05-20 17:39:36 +0000

    harpoon s/append/add and copilot chat

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -16,9 +16,20 @@ require('lazy').setup({
   -- debug
   'mfussenegger/nvim-dap',
   'mfussenegger/nvim-dap-python',
-  { 
-    "rcarriga/nvim-dap-ui", 
-    dependencies = {"mfussenegger/nvim-dap"}
+  { "rcarriga/nvim-dap-ui", dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"} },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+
+      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    },
+    opts = {
+      debug = true, -- Enable debugging
+      -- See Configuration section for rest
+    },
+    -- See Commands section for default commands if you want to lazy load on them
   },
 
 
--- lua/setup/copilot-setup.lua
+++ lua/setup/copilot-setup.lua
@@ -2,3 +2,9 @@ config = function() require('copilot').setup({
     suggestion = { enabled = false },
     panel = { enabled = false },
 }) end
+
+
+config = function() require("CopilotChat").setup {
+  debug = true, -- Enable debugging
+  -- See Configuration section for rest
+} end
